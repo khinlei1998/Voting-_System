c52d7b383821636999b4423c7aac83989b2cfe77
Yati <perthkingdom2001@gmail.com>
2020-07-19 14:13:11 +0630

    error vote10

--- database/migrations/2020_05_26_111046_create_voters_table.php
+++ database/migrations/2020_05_26_111046_create_voters_table.php
@@ -14,6 +14,7 @@ class CreateVotersTable extends Migration
     public function up()
     {
         Schema::create('voters', function (Blueprint $table) {
+            $table->bigIncrements('id');
             $table->integer('vote_id');
             $table->unsignedInteger('product_id')->nullable();
             $table->foreign('product_id')->references('product_id')->on('products');
